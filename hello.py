2c3cc720796a5362f4cb7c710de56f191509d703
Shu-Wei690 <shuweiyang690@gmail.com>
2026-09-03 22:10:42 +0800

    Updated hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,13 @@ print("=================================")
 print("Welcome here")
 print("My first post !")
 print("=================================")
+
+
+username = "shu wei"
+bio = "Love to watch cdrama"
+followers = 200
+
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
